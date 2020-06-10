adda6e467455bb47cf689dfb29dda9e873c0ba55
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-06-10 23:15:48 +0700

    Commit2 Test

--- TestUnit/Ultrasonic/Ultrasonic.ino
+++ TestUnit/Ultrasonic/Ultrasonic.ino
@@ -1,13 +1,13 @@
-//defines pins number(กำหนดหมายเลขพิน)
+//defines pins number(กำหนดขาหมายเลขPin เพื่อสั่งการทำงาน)
 const int trigPin = 6; //Ultrasonic Distance
 const int echoPin = 5; //Ultrasonic Distance
-const int busser = 9;
-const int ledPinW = 13;
-const int ledPinR = 12;
-const int ledPinWW = 10;
-const int ledPinRR = 11;
+const int busser = 9; // Active Buzzer
+const int ledPinW = 13; //ไฟสีขาวดวงที่1
+const int ledPinR = 12; //ไปสีแดงดวงที่1
+const int ledPinWW = 10; //ไฟสีขาวดวงที่2
+const int ledPinRR = 11; ////ไปสีแดงดวงที่2
 
-//defines veriables(กำหนดตัวแปร)
+//defines veriables(กำหนด/ประกาศ ตัวแปร)
 int duration; //ระยะเวลา
 int distance; //ระยะทาง
 int safetyDistance; //ระยะที่ปลอดภัย
@@ -16,15 +16,15 @@ int safetyDistance; //ระยะที่ปลอดภัย
     pinMode(trigPin, OUTPUT); // Sets the TrigPin as an Output(ให้แสดงผลค่าระยะวัตถุเข้าใกล้)
     pinMode(echoPin, INPUT); // Sets the echoPin as an Intput(ให้รับข้อมูลค่่าระยะวัตถุเข้าใกล้)
     pinMode(busser,OUTPUT); // แสดงเสียง
-    pinMode(ledPinW,OUTPUT); // แสดงไฟ
-    pinMode(ledPinR,OUTPUT);
-    pinMode(ledPinWW,OUTPUT);
-    pinMode(ledPinRR,OUTPUT);
-    Serial.begin(9600); // Starts the serial communication
+    pinMode(ledPinW,OUTPUT); // แสดงไฟสีขาว1
+    pinMode(ledPinR,OUTPUT); // แสดงไฟสีแดง1
+    pinMode(ledPinWW,OUTPUT); //แสดงไฟสีขาว2
+    pinMode(ledPinRR,OUTPUT); //แสดงไฟสีขาว2
+    Serial.begin(9600); // แสดงค่า/สื่อสารกระแสไฟ
 }
 
  void loop(){
-    //Clears the trigPin (ให้แสดงค่า Ultrasonic Distanceที่ได้เชื่อมขาไฟพิน 9 )
+    //Clears the trigPin 
     //Sets the trigPin on the 0 state for 2 micro seconds (ตั้งค่า TrigPin ในสถานะปิดเป็นเวลา 2 ไมโครวินาที)
     digitalWrite(trigPin, 0); 
     delayMicroseconds(2); //ความหน่วงเวลา
@@ -32,13 +32,13 @@ int safetyDistance; //ระยะที่ปลอดภัย
     //Sets the trigPin on the 1 state for 10 micro seconds (ตั้งค่า TrigPin ในสถานะเปิดเป็นเวลา 10 ไมโครวินาที)
     digitalWrite(trigPin, 1);
     delayMicroseconds(10);
-    digitalWrite(trigPin, 0); //แล้วก็ปิดไฟ
+    digitalWrite(trigPin, 0); //
 
     //Reads the echoPin, returns the sound wave travel time in microseconds (รับค่าหรืออ่าน echoPin ส่งคืนเวลาเดินทางของคลื่นเสียงเป็นไมโครวินาที)
     duration = pulseIn(echoPin , 1);
 
     // คำนวนค่าระยะทาง
-    distance = duration*0.034/2;
+    distance = duration*0.034/2; //(ระยะที่วัตุเข้ามาแล้วแจ้งเตือนประมาณ20-21ซม.)
 
     safetyDistance = distance; //ให้ระยะที่ปลอดภัยรับค่าจากค่าระยะทาง
     if (safetyDistance <= 20){
@@ -59,3 +59,6 @@ int safetyDistance; //ระยะที่ปลอดภัย
 Serial.print("Distance: ");
 Serial.println(distance);   
 }
+
+// ความเร็วเสียงในอากาศประมาณ 340 เมตร/วินาที
+// เวลาที่ใช้คือ ระยะทางไปกลับ ดังนั้นระยะทางคือ ครึ่งหนึ่งของที่วัดได้
