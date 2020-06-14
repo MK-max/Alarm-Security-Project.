b4d2a51162abfe194a7d6957807220a6ee65e39c
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-06-14 17:16:28 +0700

    commit 3

--- TestUnit/SevenSegment/SevenSegment.ino
+++ TestUnit/SevenSegment/SevenSegment.ino
@@ -6,31 +6,13 @@ const int DIO = 16; //ขาA2
 TM1637 sevenSegment(CLK, DIO); //Class , ชนิดobject , และส่วนที่รับพารามิเตอร์(คอนสตั๊กเกอร์)
 
 void setup() {
-  sevenSegment.init();
-  sevenSegment.set(7); // BRIGHT 0-7; //ระดับความสว่าง0-7
-
-  //sevenSegment.displayNum(10); 
-  //delay(2000);
-
-  //sevenSegment.displayNum(1234);
-  //delay(2000);
-
-  //sevenSegment.displayStr("0FF");
-  //delay(2000);
-
-  //sevenSegment.displayStr("S0S");
-  //delay(2000);
-  //sevenSegment.displayStr("HELP");
-  //delay(2000); 
+  sevenSegment.init(); 
+  sevenSegment.set(7); // BRIGHT 0-7; //ระดับความสว่าง0-7;  
 }
 
-//int number = 0;
-
 void loop() {
-  //sevenSegment.displayNum(number++);
-  //delay(100);
-  sevenSegment.displayStr("S0S");
-  delay(2000);
-  sevenSegment.displayStr("HELP");
-  delay(2000);
+  sevenSegment.displayStr("S0S"); //จอแสดงผล SOS
+  delay(2000); //หน่วงเวลา 2 วินาที
+  sevenSegment.displayStr("HELP"); //จอแสดงผล HELP
+  delay(2000); //หน่วงเวลา 2 วินาที
 }
